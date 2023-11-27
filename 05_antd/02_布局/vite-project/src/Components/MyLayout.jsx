68a5f738c914ecb26cf2310802077c12955bbe49
LeonardoSya <2246866774@qq.com>
2023-11-27 23:08:23 +0800

    modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/package.json" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/MyLayout.jsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/main.jsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/yarn.lock"

--- 05_antd/02_布局/vite-project/src/Components/MyLayout.jsx
+++ 05_antd/02_布局/vite-project/src/Components/MyLayout.jsx
@@ -1,61 +1,14 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Link, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Router, Routes, NavLink, Navigate } from 'react-router-dom';
 import { Col, Row, ColorPicker, Divider, ConfigProvider, App, Space, Select, Flex, Button, Layout, Menu, theme, Typography, Dropdown, Tooltip, Switch } from 'antd';
 import { MenuFoldOutlined, MenuUnfoldOutlined, DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined, GithubOutlined, WechatFilled } from '@ant-design/icons';
 import HomePage from './Routers/HomePage';
 import Page1 from './Routers/Page1';
+import Page21 from './Routers/Page2_1';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { Title, Paragraph } = Typography;
 
-function getItem(label, key, icon, children) {
-    return {
-        key, icon, children, label
-    };
-}
-
-const items = [
-    getItem('HomePage', '1', <PieChartOutlined />,),
-    getItem('Page 1', '2', <DesktopOutlined />),
-    getItem('Page 2', 'sub1', <UserOutlined />, [
-        getItem('Page 2-1', '3'),
-        getItem('Page 2-2', '4'),
-        getItem('Page 2-3', '5'),
-    ]),
-    getItem('Page 3', 'sub2', <TeamOutlined />, [getItem('Page 3-1', '6'), getItem('Page 3-2', '8')
-    ]),
-    getItem('Page 4', '9', <FileOutlined />),
-];
-
-const selectOptions = [
-    {
-        value: '1',
-        label: 'HomePage',
-    },
-    {
-        value: '2',
-        label: 'Page 1',
-    },
-    {
-        value: '3',
-        label: 'Page 2-1',
-    },
-    {
-        value: '4',
-        label: 'Page 2-2',
-    },
-    {
-        value: '5',
-        label: 'Page 3-1',
-    },
-    {
-        value: '6',
-        label: 'Page 4',
-    },
-]
-function toSelectedPage() {
-
-}
 
 const MyLayout = () => {
     const [collapsed, setCollapsed] = React.useState(true);
@@ -85,7 +38,7 @@ const MyLayout = () => {
                     <MySearchModule />
 
                     {/* Router */}
-
+                    <MyMap />
                     {/* Footer */}
                     <Footer style={{ textAlign: 'center', background: '#d9d9d9' }}>
                         Ecolens System ©2023 Created by Zhangyiyang
@@ -97,17 +50,58 @@ const MyLayout = () => {
     );
 }
 
+function getItem(label, key, icon, path, children) {
+    return {
+        label, key, icon, path, children
+    };
+}
+
+const items = [
+    getItem('HomePage', '1', <PieChartOutlined />, ""),
+    getItem('Page 1', '2', <DesktopOutlined />, "page1"),
+    getItem('Page 2', 'sub1', <UserOutlined />, [
+        getItem('Page 2-1', '3', "page21"),
+        getItem('Page 2-2', '4'),
+        getItem('Page 2-3', '5'),
+    ]),
+    getItem('Page 3', 'sub2', <TeamOutlined />, [
+        getItem('Page 3-1', '6', "page31"),
+        getItem('Page 3-2', '8')
+    ]),
+    getItem('Page 4', '9', <FileOutlined />),
+];
+
+// const items = [
+//     {
+//         key: '1',
+//         label: "HomePage",
+//         icon: <PieChartOutlined />,
+//         path: "",
+//     },
+//     {
+//         key: '2',
+//         label: "Page 1",
+//         icon: <DesktopOutlined />,
+//         path: "page1",
+//     },
+//     {
+//         key: '3',
+//         label: "Page 2-1",
+//         icon: <UserOutlined />,
+//         path: "page21",
+//     },
+// ]
 
 const MySider = ({ collapsed }) => (
     <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className='demo-log-vertical' />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-        {/* {items.map(item => (
-                                <Menu.Item key={item.key} icon={item.icon}>
-                                    <Link to={item.path}>{item.label}</Link>
-                                </Menu.Item>
-                            ))}
-                        </Menu> */}
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            {items.map(item => (
+                <Menu.Item key={item.key} icon={item.icon}>
+                    <Link to={item.path}>{item.label}</Link>
+                </Menu.Item>
+            ))}
+        </Menu>
     </Sider>
 );
 
@@ -136,7 +130,32 @@ const MyHeader = ({ primary, setPrimary }) => (
     </Header>
 );
 
-
+const selectOptions = [
+    {
+        value: '1',
+        label: 'HomePage',
+    },
+    {
+        value: '2',
+        label: 'Page 1',
+    },
+    {
+        value: '3',
+        label: 'Page 2-1',
+    },
+    {
+        value: '4',
+        label: 'Page 2-2',
+    },
+    {
+        value: '5',
+        label: 'Page 3-1',
+    },
+    {
+        value: '6',
+        label: 'Page 4',
+    },
+]
 const MySearchModule = ({ collapsed, setCollapsed }) => (
     <Flex justify='flex-start' align='center' gap="large" style={{ background: '#f5f5f5' }}>
         <Button
@@ -157,9 +176,24 @@ const MySearchModule = ({ collapsed, setCollapsed }) => (
                 (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
             }
             options={selectOptions}
-            onClick={toSelectedPage}
+        // ?? 研究一下选择器 API 的 Select props : https://ant-design.antgroup.com/components/select-cn#select-props
         />
     </Flex>
-)
+);
+
+const MyMap = () => (
+    <>
+        <Content>
+            {/* Route用于将应用的位置映射到不同的React组件 */}
+            {/* Route 接受 path(页面URL应导航到的路径，类似NavLink的to), element(页面导航到该路由时加载的元素) */}
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='page1' element={<Page1 />} />
+                <Route path='page21' element={<Page21 />} />
+                <Route path='*' element={<Navigate to="/" />} />
+            </Routes>
+        </Content>
+    </>
+);
 
 export default MyLayout;
