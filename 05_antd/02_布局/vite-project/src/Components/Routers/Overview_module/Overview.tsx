c3609431e385cc1923914b9aa1cdacbc1e4db756
LeonardoSya <2246866774@qq.com>
2023-12-19 17:14:13 +0800

    new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/CardComponent.tsx" 	deleted:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/LittleCard.tsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Overview.tsx"

--- 05_antd/02_布局/vite-project/src/Components/Routers/Overview_module/Overview.tsx
+++ 05_antd/02_布局/vite-project/src/Components/Routers/Overview_module/Overview.tsx
@@ -2,16 +2,48 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import G2test3d from './G2test3d';
 import Area from "./Area";
-import LittleCard from './LittleCard';
+import CardComponent from './CardComponent';
+
+import CardImage3 from '../../../assets/images/CardImage-3.png';
+import CardImage2 from '../../../assets/images/CardImage-2.png';
+import CardImage1 from '../../../assets/images/CardImage-1.png';
+import avatar from '../../../assets/images/Avatar.jpg';
+
 
 const Overview: React.FC = () => {
     return (
         <>
             <Row justify="space-evenly">
-                <Col span={5}><LittleCard /></Col>
-                <Col span={5}></Col>
-                <Col span={5}></Col>
-                <Col span={5}></Col>
+                <Col span={5}>
+                    <CardComponent
+                        title='TiTle'
+                        description='在此编辑文案'
+                        coverImage={CardImage3}
+                        avatarImage={avatar}
+                    />
+                </Col>
+                <Col span={5}>
+                    <CardComponent
+                        title='TiTle'
+                        description='在此编辑文案'
+                        coverImage={CardImage2}
+                        avatarImage={avatar}
+                    />
+                </Col>
+                <Col span={5}>
+                    <CardComponent
+                        title='TiTle'
+                        description='在此编辑文案'
+                        coverImage={CardImage1}
+                        avatarImage={avatar}
+                    /></Col>
+                <Col span={5}>
+                    <CardComponent
+                        title='TiTle'
+                        description='在此编辑文案'
+                        coverImage={CardImage3}
+                        avatarImage={avatar}
+                    /></Col>
             </Row>
             <Row justify="space-evenly">
                 <Col span={10}><G2test3d /></Col>
