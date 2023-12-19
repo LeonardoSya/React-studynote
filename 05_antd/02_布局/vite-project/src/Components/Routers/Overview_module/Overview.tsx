320368b1775e8e592944164f78e3b6c7b583c33d
LeonardoSya <2246866774@qq.com>
2023-12-19 17:14:01 +0800

    deleted:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/AnimatedLineChart.tsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Area.tsx" 	deleted:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/G2test.tsx" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/LittleCard.tsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Overview.tsx" 	deleted:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/ScatterPlot.tsx" 	deleted:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/ThumbnailAxisStackedChart.tsx" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/assets/images/Avatar.jpg" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/assets/images/CardImage-1.png" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/assets/images/CardImage-2.png" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/assets/images/CardImage-3.png" 	renamed:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/style/Loading.module.css" -> "05_antd/02_\345\270\203\345\261\200/vite-project/src/style/Overview.module.css"
    Changes not staged for commit:
    	deleted:    "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/LittleCard.tsx"
    	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Overview.tsx"
    
    Untracked files:
    	"05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/CardComponent.tsx"

--- 05_antd/02_布局/vite-project/src/Components/Routers/Overview_module/Overview.tsx
+++ 05_antd/02_布局/vite-project/src/Components/Routers/Overview_module/Overview.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Col, Row } from 'antd';
-import { } from '@ant-design/charts';
-import ThumbnailAxisStackedChart from './ThumbnailAxisStackedChart';
-import AnimatedLineChart from './AnimatedLineChart';
-import G2test from './G2test';
-import ScatterPlot from './ScatterPlot';
 import G2test3d from './G2test3d';
 import Area from "./Area";
+import LittleCard from './LittleCard';
 
 const Overview: React.FC = () => {
     return (
         <>
             <Row justify="space-evenly">
-                <Col span={5}>111</Col>
-                <Col span={5}>111</Col>
-                <Col span={5}>111</Col>
-                <Col span={5}>111</Col>
+                <Col span={5}><LittleCard /></Col>
+                <Col span={5}></Col>
+                <Col span={5}></Col>
+                <Col span={5}></Col>
             </Row>
             <Row justify="space-evenly">
                 <Col span={10}><G2test3d /></Col>
