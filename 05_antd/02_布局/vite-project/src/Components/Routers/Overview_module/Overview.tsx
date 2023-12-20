0c163ecf692cc99ab583160801fc336959852389
LeonardoSya <2246866774@qq.com>
2023-12-20 21:56:22 +0800

    modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/pnpm-lock.yaml" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Area.tsx" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/MiniArea.tsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Overview.tsx" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Overview_module/Pathln.tsx" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/Components/Routers/Page2.tsx" 	new file:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/assets/images/CardImage-4.webp" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/assets/images/IndexImage.ts" 	modified:   "05_antd/02_\345\270\203\345\261\200/vite-project/src/style/map.css"

--- 05_antd/02_布局/vite-project/src/Components/Routers/Overview_module/Overview.tsx
+++ 05_antd/02_布局/vite-project/src/Components/Routers/Overview_module/Overview.tsx
@@ -3,8 +3,10 @@ import { Col, Row } from 'antd';
 import G2test3d from './G2test3d';
 import Area from "./Area";
 import CardComponent from './CardComponent';
+import Pathln from './Pathln';
+import MiniArea from './MiniArea';
 
-import { cardImage1, cardImage2, cardImage3, avatar } from '../../../assets/images/IndexImage';
+import { cardImage1, cardImage2, cardImage3, cardImage4, avatar } from '../../../assets/images/IndexImage';
 
 
 const Overview: React.FC = () => {
@@ -15,7 +17,14 @@ const Overview: React.FC = () => {
                     <CardComponent
                         title='TiTle'
                         description='在此编辑文案'
-                        coverImage={cardImage3}
+                        coverImage={cardImage4}
+                        avatarImage={avatar}
+                    />
+                </Col>
+                <Col span={5}>
+                    <MiniArea
+                        title='TiTle'
+                        description='在此编辑文案'
                         avatarImage={avatar}
                     />
                 </Col>
@@ -23,24 +32,15 @@ const Overview: React.FC = () => {
                     <CardComponent
                         title='TiTle'
                         description='在此编辑文案'
-                        coverImage={cardImage2}
-                        avatarImage={avatar}
-                    />
-                </Col>
-                <Col span={5}>
-                    <CardComponent
-                        title='TiTle'
-                        description='在此编辑文案'
-                        coverImage={cardImage1}
-                        avatarImage={avatar}
-                    /></Col>
-                <Col span={5}>
-                    <CardComponent
-                        title='TiTle'
-                        description='在此编辑文案'
                         coverImage={cardImage3}
                         avatarImage={avatar}
                     /></Col>
+                <Col span={5}>
+                    <Pathln
+                        title='TiTle'
+                        description='在此编辑文案'
+                        avatarImage={avatar} />
+                </Col>
             </Row>
             <Row justify="space-evenly">
                 <Col span={10}><G2test3d /></Col>
